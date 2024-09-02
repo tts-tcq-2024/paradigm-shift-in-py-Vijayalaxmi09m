961a609e52e8aedbbba62bef23bccdbe18d0f70d
Vijayalaxmi09m <46927317+Vijayalaxmi09m@users.noreply.github.com>
2024-09-02 11:02:38 +0530

    Refactoring the code which contains report style functions

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,41 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+class Reporter:
+    def report(self, parameter, breach):
+        print(f"{parameter} is {breach}!")
 
-  return True
+class BatteryChecker:
+    def __init__(self, reporter=None):
+        self.reporter = reporter or Reporter()
 
+    def check_parameter(self, parameter_name, value, min_value, max_value):
+        if value < min_value:
+            self.reporter.report(parameter_name, 'too low')
+            return False
+        if value > max_value:
+            self.reporter.report(parameter_name, 'too high')
+            return False
+        return True
+
+    def battery_is_ok(self, temperature, soc, charge_rate):
+        return (
+            self.check_parameter('Temperature', temperature, 0, 45) and
+            self.check_parameter('State of Charge', soc, 20, 80) and
+            self.check_parameter('Charge Rate', charge_rate, 0, 0.8)
+        )
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    checker = BatteryChecker()
+    
+    # Complete tests covering all scenarios
+    assert(checker.battery_is_ok(25, 70, 0.7) is True)
+    assert(checker.battery_is_ok(50, 85, 0) is False)   # Temperature too high, SOC too high
+    assert(checker.battery_is_ok(-5, 70, 0.5) is False)  # Temperature too low
+    assert(checker.battery_is_ok(30, 85, 0.5) is False)  # SOC too high
+    assert(checker.battery_is_ok(30, 70, 0.9) is False)  # Charge rate too high
+    assert(checker.battery_is_ok(25, 70, 0.8) is True)   # All within range
+    assert(checker.battery_is_ok(0, 20, 0.8) is True)    # Boundary conditions lower
+    assert(checker.battery_is_ok(45, 80, 0.8) is True)   # Boundary conditions upper
+    assert(checker.battery_is_ok(0, 19, 0.8) is False)   # SOC too low
+    assert(checker.battery_is_ok(0, 20, 0.9) is False)   # Charge rate too high
+
+
