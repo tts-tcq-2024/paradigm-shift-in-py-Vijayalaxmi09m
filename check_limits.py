2dfd3e7e8e961de4436a2b1a78ab05b061779c4a
Vijayalaxmi09m <46927317+Vijayalaxmi09m@users.noreply.github.com>
2024-10-16 16:33:09 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,9 +1,11 @@
 class Reporter:
+    """Base class for all reporters."""
     def report(self, parameter, message):
         raise NotImplementedError("Subclasses should implement this method.")
 
 
 class ListReporter(Reporter):
+    """Reporter that stores reports in memory for testing."""
     def __init__(self):
         self.reports = []
 
@@ -12,6 +14,7 @@ class ListReporter(Reporter):
 
 
 class FileReporter(Reporter):
+    """Reporter that logs reports to a file."""
     def __init__(self, file_name):
         self.file_name = file_name
 
@@ -21,39 +24,48 @@ class FileReporter(Reporter):
 
 
 class BatteryChecker:
+    """Class to check battery parameters and generate reports."""
+
     def __init__(self, reporter=None):
         self.reporter = reporter or Reporter()
 
-    def _calculate_warning_range(self, min_value, max_value, tolerance_percent):
-        """Calculate the warning range based on the tolerance."""
-        tolerance = tolerance_percent / 100 * max_value
-        return (min_value, min_value + tolerance), (max_value - tolerance, max_value)
+    def _calculate_tolerance(self, max_value, tolerance_percent=5):
+        """Calculate tolerance based on the percentage of the max value."""
+        return (tolerance_percent / 100) * max_value
 
-    def _check_parameter(self, parameter_name, value, min_value, max_value, enable_warning):
-        """Check if the value is within limits and report warnings if enabled."""
+    def _within_warning_range(self, value, min_value, max_value, tolerance):
+        """Check if value falls within the warning range."""
+        low_warning, high_warning = min_value + tolerance, max_value - tolerance
+        if min_value <= value < low_warning:
+            return "Approaching discharge"
+        elif high_warning < value <= max_value:
+            return "Approaching charge-peak"
+        return None
+
+    def _check_value(self, parameter_name, value, min_value, max_value, enable_warning):
+        """Evaluate if the parameter value is within acceptable limits."""
         if value < min_value:
             self.reporter.report(parameter_name, "too low")
             return False
-        if value > max_value:
+        elif value > max_value:
             self.reporter.report(parameter_name, "too high")
             return False
 
         if enable_warning:
-            low_warning, high_warning = self._calculate_warning_range(min_value, max_value, 5)
-            if low_warning[0] <= value < low_warning[1]:
-                self.reporter.report(parameter_name, "Approaching discharge")
-            if high_warning[0] < value <= high_warning[1]:
-                self.reporter.report(parameter_name, "Approaching charge-peak")
+            tolerance = self._calculate_tolerance(max_value)
+            warning_message = self._within_warning_range(value, min_value, max_value, tolerance)
+            if warning_message:
+                self.reporter.report(parameter_name, warning_message)
 
         return True
 
     def battery_is_ok(self, temperature, soc, charge_rate):
-        """Main function to validate battery parameters."""
-        return (
-            self._check_parameter('Temperature', temperature, 0, 45, enable_warning=True) and
-            self._check_parameter('State of Charge', soc, 20, 80, enable_warning=True) and
-            self._check_parameter('Charge Rate', charge_rate, 0, 0.8, enable_warning=False)
-        )
+        """Check all battery parameters."""
+        return all([
+            self._check_value('Temperature', temperature, 0, 45, enable_warning=True),
+            self._check_value('State of Charge', soc, 20, 80, enable_warning=True),
+            self._check_value('Charge Rate', charge_rate, 0, 0.8, enable_warning=False)
+        ])
 
 
 if __name__ == '__main__':
@@ -61,7 +73,7 @@ if __name__ == '__main__':
     list_reporter = ListReporter()
     checker = BatteryChecker(list_reporter)
 
-    assert checker.battery_is_ok(22, 21, 0.7) is True  # Warning for SOC should be reported
+    assert checker.battery_is_ok(22, 21, 0.7) is True  # Warning for SOC
     assert checker.battery_is_ok(50, 85, 0) is False    # Temperature & SOC too high
 
     print("Stored reports:", list_reporter.reports)
